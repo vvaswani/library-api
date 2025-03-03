1e7cb27276db1eb7cddc4491b0d1afa2a7a2708c
Vikram Vaswani <vikram@dagger.io>
2025-03-03 19:06:13 +0000

    Update agent
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- agent/src/agent/main.py
+++ agent/src/agent/main.py
@@ -6,14 +6,14 @@ from dagger import dag, function, object_type
 class Agent:
     @function
     async def heal(self, source: dagger.Directory) -> dagger.Container:
-        ws = dag.workspace(source)
+        ws = dag.workspace(source=source)
 
         prompt = f"""
         You are an expert in the Python FastAPI framework. You understand the framework and its ecosystem. You have a deep understanding of the FastAPI lifecycle and can build complex applications with ease. You are comfortable with the command line and can navigate the FastAPI directory structure with ease.
 
         The tests in this FastAPI application are failing. Use the 'write_file', 'diff', 'test' tools from the workspace only. You need to identify the error(s) that prevent the unit tests from passing and propose changes to ensure all tests pass. You have been given the following additional instructions:
 
-        - Only consider Python files in the /app directory for this task. Ignore any and all syntax errors you may find.
+        - Only consider Python files in the /app directory for this task.
         - Avoid unnecessary or unrelated changes. Make the smallest possible change that meets the goal
         - Always write changes to files to the original files
         - Only use the 'test' tool to run all tests. Do not use 'pytest' or any other testing tool.
@@ -22,7 +22,7 @@ class Agent:
         - Only use the 'write_file' tool to write your changes to the files
         - Before using the 'write_file' tool, use the 'test' tool to check that all changes are correct
 
-        Do not explain your process or reasoning. Only make the changes needed to make the tests pass.
+        Explain your process or reasoning. Make the changes needed to make the tests pass.
         """
         after = await (
             dag.llm()
--- agent/workspace/src/workspace/main.py
+++ agent/workspace/src/workspace/main.py
@@ -1,28 +1,82 @@
-import dagger
-from dagger import dag, function, object_type
-
+from typing import Annotated, Self
+from dagger import Container, dag, Directory, Doc, function, object_type, ReturnType
 
 @object_type
 class Workspace:
-    source: dagger.Directory
+    ctr: Container
+    source: Directory
+    last_exec_output: str
 
     @classmethod
     async def create(
         cls,
-        source: dagger.Directory,
+        source: Annotated[Directory, Doc("The context for the workspace")] = dag.directory(),
     ):
-        cls = (
-            dag.container()
+        ctr = (
+            dag
+            .container()
             .from_("python:3.11")
-            .with_directory("/app", source)
             .with_workdir("/app")
+            .with_directory("/app", source)
             .with_mounted_cache("/root/.cache/pip", dag.cache_volume("python-pip"))
             .with_exec(["pip", "install", "-r", "requirements.txt"])
         )
-        return cls(source=source)
+        return cls(ctr=ctr, source=source, last_exec_output="")
 
     @function
-    async def test(self) -> str:
+    async def read_file(
+        self,
+        path: Annotated[str, Doc("File path to read a file from")]
+    ) -> str:
+        """Returns the contents of a file in the workspace at the provided path"""
+        return await self.ctr.file(path).contents()
+
+    @function
+    def write_file(
+        self,
+        path: Annotated[str, Doc("File path to write a file to")],
+        contents: Annotated[str, Doc("File contents to write")]
+    ) -> Self:
+        """Writes the provided contents to a file in the workspace at the provided path"""
+        self.ctr = self.ctr.with_new_file(path, contents)
+        return self
+
+    @function
+    def write_directory(
+        self,
+        path: Annotated[str, Doc("Directory path to write a directory to")],
+        dir: Annotated[Directory, Doc("Directory contents to write")]
+    ) -> Self:
+        """Writes the provided contents to a directory in the workspace at the provided path"""
+        self.ctr = self.ctr.with_directory(path, dir)
+        return self
+
+    @function
+    async def ls(
+        self,
+        path: Annotated[str, Doc("Path to get the list of files from")]
+    ) -> list[str]:
+        """Returns the list of files in the workspace at the provided path"""
+        return await self.ctr.directory(path).entries()
+
+    @function
+    async def check(
+        self
+    ) -> str:
+        """Checks if the workspace meets the requirements"""
+        cmd = (
+            self.ctr
+            .with_exec(["sh", "-c", self.checker], expect=ReturnType.ANY)
+        )
+        out = await cmd.stdout() + "\n\n" + await cmd.stderr()
+        if await cmd.exit_code() != 0:
+            raise Exception(f"Checker failed: {self.checker}\nError: {out}")
+        return out
+
+    @function
+    async def test(
+        self
+    ) -> str:
         postgresdb = (
             dag.container()
             .from_("postgres:alpine")
@@ -36,14 +90,49 @@ class Workspace:
             self.ctr
             .with_service_binding("db", postgresdb)
             .with_env_variable("DATABASE_URL", "postgresql://postgres:secret@db/app_test")
-            .with_exec(["pytest", "--tb=line"])
+            .with_exec(["pytest"])
             .stdout()
         )
 
-    @function
-    async def write_file(self, path: str, contents: str) -> dagger.File:
-        return await self.ctr.with_new_file(path, contents).file(path)
 
     @function
-    async def diff(self) -> str:
-        return await self.ctr.with_exec(["git", "diff"]).stdout()
+    async def diff(
+        self
+    ) -> str:
+        """Returns the changes in the workspace so far"""
+        source = dag.container().from_("alpine/git").with_workdir("/app").with_directory("/app", self.source)
+        # make sure source is a git directory
+        if ".git" not in await self.source.entries():
+            source = source.with_exec(["git", "init"]).with_exec(["git", "add", "."]).with_exec(["git", "commit", "-m", "'initial'"])
+        # return the git diff of the changes in the workspace
+        return await source.with_directory(".", self.ctr.directory(".")).with_exec(["git", "diff"]).stdout()
+
+    @function
+    async def exec(
+        self,
+        command: Annotated[str, Doc("command to execute in the workspace")]
+    ) -> Self:
+        """Executes a command in the workspace. Does not return the output of the command"""
+        cmd = (
+            self.ctr
+            .with_exec(["sh", "-c", command], expect=ReturnType.ANY)
+        )
+        if await cmd.exit_code() != 0:
+            raise Exception(f"Command failed: {command}\nError: {await cmd.stderr()}")
+        self.ctr = cmd # FIXME
+        self.last_exec_output = await cmd.stdout()
+        return self
+
+    @function
+    def get_exec_output(
+        self
+    ) -> str:
+        """Returns the output of the last executed command"""
+        return self.last_exec_output
+
+    @function
+    def container(
+        self
+    ) -> Container:
+        """Returns the container for the workspace"""
+        return self.ctr
